00e1487f84dcf55292e3a6bfe2a6b3f8e2e8188d
vsedmik <vsedmik@redhat.com>
2022-04-20 09:51:21 +0200

    add tests for puppet availability on upgraded setup

--- pytest_fixtures/core/upgrade.py
+++ pytest_fixtures/core/upgrade.py
@@ -1,5 +1,8 @@
 import pytest
 
+from robottelo.config import settings
+from robottelo.hosts import Capsule
+
 
 @pytest.fixture(scope="function")
 def dependent_scenario_name(request):
@@ -12,3 +15,14 @@ def dependent_scenario_name(request):
         if 'depend_on' in mark.kwargs
     ][0]
     yield depend_test_name
+
+
+@pytest.fixture(scope='session', params=[True, False], ids=["satellite", "capsule"])
+def upgrade_server(request, default_sat):
+    """
+    This fixture is used to return the upgrade Satellite and Capsule instances.
+    """
+    if request.param:
+        return default_sat
+    else:
+        return Capsule(settings.upgrade.capsule_hostname)
